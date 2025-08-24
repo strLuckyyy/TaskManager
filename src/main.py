f80aa204d6ce8b56d6da68fb0a87c1d07d8323fe
strLuckyyy <abrahaofcon@gmail.com>
2025-08-24 16:28:05 -0300

    Implement SQLite database for task management with CRUD operations

--- src/main.py
+++ src/main.py
@@ -1,47 +1,36 @@
+# src/main.py
 from fastapi import FastAPI, HTTPException
-from src.model import CreateTask, Task, Status
-
+from src.model import CreateTask, Task
+from src.db import Database
 
 app = FastAPI()
-
-tasks: list[Task] = []
-task_id_counter = 1
+db = Database()  # instância do SQLite
 
 @app.post("/tasks/", response_model=Task)
 def create_task(task: CreateTask):
-    global task_id_counter
-    new_task = Task(id=task_id_counter, **task.model_dump())
-    tasks.append(new_task)
-    task_id_counter += 1
-    return new_task
+    return db.add_task(task)
 
 @app.get("/tasks/", response_model=list[Task])
 def get_tasks():
-    return tasks
+    return db.get_tasks()
 
 @app.get("/tasks/{task_id}", response_model=Task)
 def get_task(task_id: int):
-    for task in tasks:
-        if task.id == task_id:
-            return task
-    raise HTTPException(status_code=404, detail="Task not found")
+    task = db.get_task_by_id(task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task
 
 @app.put("/tasks/{task_id}", response_model=Task)
 def update_task(task_id: int, updated_task: CreateTask):
-    for i, task in enumerate(tasks):
-        if task.id == task_id:
-            task_data = updated_task.model_dump()
-            task_data["completed"] = task_data["status"] == Status.DONE
-
-            new_task = Task(id=task_id, **task_data)
-            tasks[i] = new_task
-            return new_task
-    raise HTTPException(status_code=404, detail="Task not found")
+    task = db.update_task(task_id, updated_task)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    for i, task in enumerate(tasks):
-        if task.id == task_id:
-            tasks.pop(i)
-            return {"detail": "Task deleted"}
-    raise HTTPException(status_code=404, detail="Task not found")
+    success = db.delete_task(task_id)
+    if not success:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return {"detail": "Task deleted"}
